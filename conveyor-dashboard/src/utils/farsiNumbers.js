46c60e70a179d21d4c905a6f8692751a42607391
amir behvandi <amirbehvandi747@gmail.com>
2025-12-01 13:10:50 +0330

    update nginx for http

--- conveyor-dashboard/src/utils/farsiNumbers.js
+++
@@ -1,9 +0,0 @@
-// src/utils/farsiNumbers.js
-export const toFarsiNumber = (number) => {
-  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-  return number.toString().replace(/\d/g, x => farsiDigits[x]);
-};
-
-export const formatCurrency = (amount) => {
-  return `${toFarsiNumber(amount.toLocaleString())} تومان`;
-};
